c6228c036bddda60196f5fd4cf3bbaa7bffbb58e
Júlio César Queiroz Carvalho <113850389+julio7745@users.noreply.github.com>
2023-01-24 08:45:20 -0300

    update

--- scripts/home.js
+++ scripts/home.js
@@ -1,16 +1,14 @@
-//setTimeout(mudafoto, 2000);
-
 var slide = window.document.getElementsByClassName('slide')
-var margin = 0 
+var margin = 0
+
+setInterval(mudafoto, 4000)
 
 function mudafoto(){
     
-   if (margin == ((slide.length * (-slide[0].clientWidth)) + (slide[0].clientWidth))){
+    if (margin == -(slide.length -1)){
         margin = 0
     }else{
-        margin -= slide[0].clientWidth
+        margin -= 1
     }
-    slide[0].style.marginLeft = `${margin}px`
+    slide[0].style.marginLeft = `${margin * (slide[0].clientWidth)}px`
 }
-
-setInterval(mudafoto, 4000);
