b57725173fcc9bd11933e52bb8acd49d7887eb62
Júlio César Queiroz Carvalho <113850389+julio7745@users.noreply.github.com>
2023-02-14 16:07:09 -0300

    copo da felicidade brownie + alterações docinho

--- scripts/feitoPorBia.js
+++ scripts/feitoPorBia.js
@@ -89,25 +89,6 @@ function config() {
     `
 }
 
-//PRODUTOS --> PAGINA PRODUTOS
-function load(){
-
-    var param = new URLSearchParams(window.location.search).get('p')
-    var produto = window.document.getElementsByClassName('container')
-    var bolo = window.document.getElementsByClassName('bolo')
-    
-    switch (param) {
-        case '1':
-            for(let aux in produto){
-                produto[aux].style.display='none'
-            }
-            for(let aux in bolo){
-                bolo[aux].style.display='inline-block'
-            }
-            break;
-    }
-}
-
 //MENU
 function abremenu(){
     menu.style.marginLeft = 0
---
+++ scripts/produtos.js
@@ -0,0 +1,19 @@
+//PRODUTOS --> PAGINA PRODUTOS
+function load(){
+
+    let param = new URLSearchParams(window.location.search).get('p')
+    let produto = window.document.getElementsByClassName('container')
+    console.log("PRODUTO");
+    let bolo = window.document.getElementsByClassName('bolo')
+    
+    switch (param) {
+        case '1':
+            for(let aux = 0; aux < produto.length; aux++){
+                produto[aux].style.display='none'
+            }
+            for(let aux = 0; aux < bolo.length; aux++){
+                bolo[aux].style.display='inline-block'
+            }
+            break;
+    }
+}
